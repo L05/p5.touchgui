11a42a47ba18dd85250ce489beecb777ea052054
L05 <carlosga@gmail.com>
2019-03-03 02:13:26 -0800

    Changed structure, added ToggleButton
    Changed from class to function structure so that the inheritance would work from Button to MomentaryButton and ToggleButton (which is new).

--- examples/sketch.js
+++ examples/sketch.js
@@ -1,15 +1,24 @@
-let mb;
+let mb, tb, state;
 
 function setup() {
   createCanvas(400, 400);
   background(100, 0, 0);
   
-  mb = new MomentaryButton(200, 200, 50, 50, "X", myCallback);
+  mb = new MomentaryButton(100, 200, 100, 100, "X", myCallback1);
+  mb.setCallback(myCallback2);
   mb.setRound(10);
   
+  // There are 3 modes for a momentary button
   mb.setMode(DOWN);
-//  mb.setMode(UP);
-//  mb.setMode(HOLD);
+  //  mb.setMode(UP);
+  //  mb.setMode(HOLD);
+  
+  tb = new ToggleButton(300, 200, 100, 100, "X", myCallback3, myCallback4);
+  tb.setRound(10);
+  
+  // There are 2 modes for a toggle button
+  tb.setMode(DOWN);
+  //  tb.setMode(UP);
 }
 
 function draw() {
@@ -17,11 +26,32 @@ function draw() {
   background(100, 0, 0, 32);
   
   mb.draw();
+  tb.draw();
+  
+  if (state) {
+    fill(255);
+    textAlign(CENTER);
+    text("BUTTON TOGGLED.", 300, 350);
+  }
 }
 
-function myCallback() {
+// Example callback functions
+function myCallback1() {
+  print("BUTTON PRESSED.");
+}
+
+function myCallback2() {
   fill(255);
   textAlign(CENTER);
-  text("BUTTON PRESSED.", 200, 300);
-  print("BUTTON PRESSED.");
+  text("BUTTON PRESSED.", 100, 350);
+}
+
+function myCallback3() {
+  state = true;
+  print("BUTTON TOGGLED ON.");
+}
+
+function myCallback4() {
+  state = false;
+  print("BUTTON TOGGLED OFF.");
 }
