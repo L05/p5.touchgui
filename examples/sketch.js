b265db57b0f656bbf533fa91dc77e014674f22c5
L05 <carlosga@gmail.com>
2019-03-03 13:47:10 -0800

    Added uiObject parent class
    Added uiObject parent class for further extensibility, which also cleans up child classes.
    Cleaned up example file to be simpler.

--- examples/sketch.js
+++ examples/sketch.js
@@ -1,49 +1,47 @@
-let mb, tb, state;
+let mb, tb, tbState;
 
 function setup() {
   createCanvas(400, 400);
   background(100, 0, 0);
   
+  // create momentary button
   mb = createMomentary(100, 100, 100, 50, "X", momentaryCallback);
-  mb.setPosition(100, 200);
-  mb.setSize(100, 100);
-  mb.setRound(10);
-  // There are 3 modes for a momentary button: DOWN, UP, HOLD
-  mb.setMode(DOWN);
+  mb.position(100, 200);
+  mb.size(100, 100);
+  mb.round(10);
+  mb.touchMode(DOWN); // options: DOWN, UP, HOLD
   
+  // create toggle button
   tb = createToggle(300, 200, 100, 100, "X", toggleOn, toggleOff);
-  // There are 2 modes for a toggle button: DOWN, UP
-  tb.setMode(DOWN);
+  tb.touchMode(DOWN); // options: DOWN, UP
+  
+  // styling for text popups
+  fill(255);
+  textAlign(CENTER);
 }
 
 function draw() {
   background(100, 0, 0, 32);
   
-  // Simply call a button's draw() function for it to 
-  // update and display
   mb.draw();
   tb.draw();
   
-  if (state) {
-    fill(255);
-    textAlign(CENTER);
+  if (tbState) {   
     text("BUTTON TOGGLED.", 300, 350);
   }
 }
 
-// Example callback functions
+// callback functions
 function momentaryCallback() {
-  fill(255);
-  textAlign(CENTER);
   text("BUTTON PRESSED.", 100, 350);
 }
 
 function toggleOn() {
-  state = true;
+  tbState = true;
   print("BUTTON TOGGLED ON.");
 }
 
 function toggleOff() {
-  state = false;
+  tbState = false;
   print("BUTTON TOGGLED OFF.");
 }
