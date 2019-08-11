1bf06ea5c2a796dda093f1a9a0f245a97f48e695
Yining Shi <ys2189@nyu.edu>
2019-08-11 16:55:48 -0400

    adding 3 examples

---
+++ examples/drawing-app/sketch.js
@@ -0,0 +1,124 @@
+const myCanvasPos = {
+  x: 20,
+  y: 20,
+  width: 300,
+  height: 300
+};
+let gui;
+let myStrokeWeight = 16;
+let myRed = 127.5, myGreen = 127.5, myBlue = 127.5;
+
+// Create variable for clear button
+let myClearButton;
+// Create variable for stroke weight slider
+let myWeightSlider;
+// Create variableS for stroke color r,g,b,a
+let myColorSliderR, myColorSliderG, myColorSliderB;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(225);
+  ellipseMode(CENTER);
+
+  gui = createGui();
+  // Set style to Blue!
+  gui.style.Blue();
+
+  // Create buttons, sliders...
+  createControls();
+}
+
+function createControls() {
+  strokeWeight(1);
+  // Create drawing canvas
+  rect(myCanvasPos.x, myCanvasPos.y, myCanvasPos.width, myCanvasPos.height);
+
+  // Optional: a way to set the font size, maybe use .labelFontSize?
+  gui.style.labelFontMaxSize = 12;
+  // Optional: A way to change the buttonStroke, maybe bStrokeWt to buttonStrokeWeight?
+  gui.style.bStrokeWt = 1;
+  // Optional: A way to set button corners?
+  gui.style.rounding = 0;
+
+  // Create Clear Button
+  myClearButton = createButton("Clear", 330, 20, 195, 30);
+
+  // Optional: control slider weight? maybe sStrokeWt to sliderStrokeWeight
+  gui.style.sStrokeWt = 1;
+
+  text('Stroke Weight:', 330, 65);
+  // Create stroke weight slider
+  myWeightSlider = createSlider("WeightSlider", 330, 70, 160, 30, 1, 30);
+
+  text('Red:', 330, 115);
+  text('Green:', 385, 115);
+  text('Blue:', 440, 115);
+  myColorSliderR = createSliderV("ColoSliderR", 330, 120, 30, 200, 0, 255);
+  myColorSliderG = createSliderV("ColoSliderG", 385, 120, 30, 200, 0, 255);
+  myColorSliderB = createSliderV("ColoSliderB", 440, 120, 30, 200, 0, 255);
+  
+  // Show an ellipse with myStrokeWeight and myColor on the canvas
+  updateEllipse();
+}
+
+
+function draw() {
+  drawGui();
+  if (myClearButton.pressed) {
+    // Print a message when Clear Button is pressed.
+    print(myClearButton.label + " pressed.");
+    fill(255);
+    stroke(0);
+    strokeWeight(1);
+    // Create drawing canvas again
+    rect(myCanvasPos.x, myCanvasPos.y, myCanvasPos.width, myCanvasPos.height);
+  }
+  if (myWeightSlider.changed) {
+    // Print a message when myWeightSlider Slider is changed
+    print(myWeightSlider.label + " = " + myWeightSlider.val);
+    // Set myStrokeWeight to the slides' value
+    myStrokeWeight = myWeightSlider.val;
+    // Update the myStrokeWeight value on the canvas
+    updateEllipse();
+  }
+  if (myColorSliderR.changed) {
+    print(myColorSliderR.label + " = " + myColorSliderR.val);
+    myRed = myColorSliderR.val;
+    updateEllipse();
+  }
+  if (myColorSliderG.changed) {
+    print(myColorSliderG.label + " = " + myColorSliderG.val);
+    myGreen = myColorSliderG.val;
+    updateEllipse();
+  }
+  if (myColorSliderB.changed) {
+    print(myColorSliderB.label + " = " + myColorSliderB.val);
+    myBlue = myColorSliderB.val;
+    updateEllipse();
+  }
+  // If mouse is pressed inside of the drawing canvas, draw lines
+  if (mouseIsPressed) {
+    if (ifMouseInCanvas(pmouseX, pmouseY, mouseX, mouseY)) {
+      stroke(myRed, myGreen, myBlue);
+      strokeWeight(myStrokeWeight);
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
+}
+
+function updateEllipse() {
+  noStroke();
+  fill(225);
+  ellipse(512, 85, 30, 30);
+  fill(myRed, myGreen, myBlue);
+  ellipse(512, 85, myStrokeWeight, myStrokeWeight);
+}
+
+function ifMouseInCanvas(pmouseX, pmouseY, mouseX, mouseY) {
+  if (mouseX > myCanvasPos.x + myStrokeWeight / 2 && mouseX < myCanvasPos.x + myCanvasPos.width - myStrokeWeight / 2 &&
+      mouseY > myCanvasPos.y + myStrokeWeight / 2 && mouseY < myCanvasPos.y + myCanvasPos.height - myStrokeWeight / 2 && 
+      pmouseX > myCanvasPos.x + myStrokeWeight / 2 && pmouseX < myCanvasPos.x + myCanvasPos.width - myStrokeWeight / 2 &&
+      pmouseY > myCanvasPos.y + myStrokeWeight / 2 && pmouseY < myCanvasPos.y + myCanvasPos.height - myStrokeWeight / 2) {
+    return true;
+  } else return false;
+}
---
+++ examples/slider2/sketch.js
@@ -0,0 +1,36 @@
+let gui;
+
+// Create a variable for your slider
+let s;
+
+function setup() {
+  createCanvas(400, 400);
+  gui = createGui();
+  
+  // Create Slider 1.
+  // The last two optional arguments define the min and max (minimum and maximum) values.
+  // The default min and max values are 0 and 1, respectively.
+  s = createSlider("Slider 1", 50, 50, 300, 32, 100, 300);
+}
+
+function draw() {
+  background(220);
+  drawGui();
+  
+  if (s.changed) {
+    // Print a message when Slider 1 is changed
+    // that displays its value.
+    print(s.label + " = " + s.val);
+  }
+  
+  // Use Slider 1's value to determine where the ellipse is drawn.
+  fill(255, 0, 0);
+  ellipseMode(CENTER);
+  ellipse(195, 240, s.val, s.val);
+}
+
+/// Add these lines below sketch to prevent scrolling on mobile
+function touchMoved() {
+  // do some stuff
+  return false;
+}
